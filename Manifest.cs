e7930e652dcc3c7315bb9763c374d8e7dce12e0e
Peter Keating <pete.keating@etchuk.com>
2021-04-30 12:31:36 +0100

    Upgrade to RC2
    Added documentation on features provided by this module.

--- Manifest.cs
+++ Manifest.cs
@@ -5,6 +5,6 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Add postcode search functionality.",
     Name = "Postcode search",
-    Version = "0.0.1",
+    Version = "0.1.0",
     Website = "https://etchuk.com"
 )]
