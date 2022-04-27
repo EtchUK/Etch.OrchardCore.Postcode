bfb2e25edf58f180698a4d644de57e4707f347a8
Mehdi Miah <Mehdi.Miah@etchuk.com>
2022-04-27 16:43:59 +0100

    Upgrade to orchard core 1.3.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,6 +5,6 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Add postcode search functionality.",
     Name = "Postcode search",
-    Version = "0.2.0",
+    Version = "1.2.0",
     Website = "https://etchuk.com"
 )]
